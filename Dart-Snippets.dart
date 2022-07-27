69fbd05f60adc6b5548a240a02ee6221c93f1aa7
Muhammad Afzal <77921765+imafzalakram@users.noreply.github.com>
2022-07-27 14:20:31 +0500

    Update Dart-Snippets.dart

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,3 +1,13 @@
 void main() {
-  print("Hello World!");
+  //Syntax to create statically (explicitly) typed variables
+  int number = 122;
+  String name = "Ali";
+  double gpa = 3.36;
+  bool check = false;
+
+  //Syntax to create Inferred variables
+  var variable1 = 122;
+  var variable2 = "Ali";
+  var variable3 = 3.36;
+  var variable4 = false;
 }
