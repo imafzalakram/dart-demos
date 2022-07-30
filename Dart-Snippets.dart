4cefdd61bd81311ff8d940d6ca56736ff659b151
Muhammad Afzal <imafzalakram@gmail.com>
2022-07-30 14:28:03 +0500

    String concatenation

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,3 +1,14 @@
 void main() {
-  print("Hello World!");
+  var studentName = "Ali";
+  double studentGPA = 3.36;
+
+  print("Student name is $studentName and GPA is " + studentGPA.toString());
+
+  // String Concatenation
+  // concatenate 'string' with 'another string'
+  // concatenate 'string' with 'integer-variable'
+  // concatenate 'string' with 'double-variable'
+  // concatenate 'string' with 'boolean-variable'
+
+  // we use plus (+) sign to concatenate strings with other element
 }
