4463441e208b0b62cdc53406398e0cc3ab09542e
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-02 12:59:34 +0500

    Upgraded

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,5 +1,5 @@
 //Program to convert Celsuis to Fahrenheit
 void main() {
   double celsuisTemp = 33.23;
-  print(" $celsuisTemp is ${(celsuisTemp*9/5)+32}");
+  print(" ${celsuisTemp}°C is ${(celsuisTemp*9/5)+32}°F");
 }
