1e02891bf0dc69cb98b45232504fd15d527243d2
Muhammad Afzal <77921765+imafzalakram@users.noreply.github.com>
2022-08-02 12:20:10 +0500

    Add files via upload

---
+++ Dart-Snippets.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Create a dart program that will create two variables
+  // and they will perform 'Addition','Subtraction' 'Multplication' & 'Division'
+  
+  var no1 = 45, no2= 56;
+  print(" The addition of $no1 & $no2 is ${(no1+no2)} ");
+  print(" The substraction of $no1 & $no2 is ${no1-no2}");
+  print(" The division of $no1 and $no2 is ${no1/no2}");
+  print (" The product of $no1 and $no2 is ${no1*no2}");
+}
