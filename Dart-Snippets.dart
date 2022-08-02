7a50d18beefa10c4c532f0cdf23420a6eefdcdd5
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-02 12:25:38 +0500

    Dart program to convert Celsuis to Fahrenheit

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,17 +1,5 @@
+//Program to convert Celsuis to Fahrenheit
 void main() {
-  
-  var userId = 112;
-/* 
-  Variable naming rules
-  -Variable names can contain alphabets (Small capital), numbers, $ sign, underscore -
-  -Variabel name must began with alphabets, $, -, we can't start variable name with numbers
-  -We can't use keywords as variable names
-  -We can't use space or any special chrachters inside a variable name
-
-  Variable naming conventions
-  -Variable name should be descriptive
-  -Variable name should written in camelcases
-  -Variable name should be short in length
-*/
-
+  double celsuisTemp = 33.23;
+  print(" $celsuisTemp is ${(celsuisTemp*9/5)+32}");
 }
