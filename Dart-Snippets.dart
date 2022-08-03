5c1611c3c5dac8b46cd57b89e572e8c057655656
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-03 17:08:29 +0500

    Nested if else statement

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,14 +1,25 @@
 void main() {
-  // if else-if statement
-
+  // Nested if else syntax and its implementation 
   bool isRaining = false;
   bool isSnowing = true;
 
+  int temp = 23;
+
   if (isRaining) {
-    print("Wear Jacket");
+    if (temp > 35) {
+      print("Wear light weight jacket");
+    } else if (temp > 25) {
+      print(" Wear rain coat");
+    } else {
+      print("Wear Coat");
+    }
   } else if (isSnowing) {
-    print("Wear Coat");
-  } else {
-    print("Wear casual dress");
+    if (temp > 25) {
+      print("Soft jacket with cap");
+    } else if (temp > 10) {
+      print("Wear Coat");
+    } else if (temp < 10) {
+      print(" Wear Leather Jacket ");
+    }
   }
 }
