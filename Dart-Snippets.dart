ccc734364b467cb683c4ad1bf157455e85cabb38
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-03 22:11:22 +0500

    Shorthand code for if else statement

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,25 +1,14 @@
+// Program to use shorthand code for if else 
 void main() {
-  // Nested if else syntax and its implementation 
-  bool isRaining = false;
-  bool isSnowing = true;
-
-  int temp = 23;
+  bool isRaining = true;
 
   if (isRaining) {
-    if (temp > 35) {
-      print("Wear light weight jacket");
-    } else if (temp > 25) {
-      print(" Wear rain coat");
-    } else {
-      print("Wear Coat");
-    }
-  } else if (isSnowing) {
-    if (temp > 25) {
-      print("Soft jacket with cap");
-    } else if (temp > 10) {
-      print("Wear Coat");
-    } else if (temp < 10) {
-      print(" Wear Leather Jacket ");
-    }
+    print("Wear rain coat!");
+  } else {
+    print("Casual dress!");
   }
+
+  // Short-Hand Code for above use case
+  // condition ? statement1 : statement2;
+  isRaining ? print("Wear rain coat!") : print("Casual dress!");
 }
