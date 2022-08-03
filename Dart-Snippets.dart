4ef7a687ba41307ce82dfd9f5e2820047f05bd2f
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-03 16:58:17 +0500

    Simple if else statement

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,6 +1,9 @@
-//Write a program to print values of two integer variables in single linevoid main() {
 void main() {
-  int no1 = 312;
-  int no2 = 532;
-  print("No1 is $no1 and No2 is $no2");
+  // Simple if else statement
+  var wantToLearnFlutter = false;
+  if (wantToLearnFlutter) {
+    print("Learn Dart first!");
+  } else {
+    print("Learn somehting else");
+  }
 }
