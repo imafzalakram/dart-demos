22bc3f93c322ff2c802ccad0f382c9b96bb38257
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-04 13:09:32 +0500

    Syntax for Increment (pre-increment, post-increment) & Decrement (pre-decrement, post-decrement) operators

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,10 +1,21 @@
 void main() {
-  // Program to implement Arithmatic Operators
-  int no1 = 23, no2 = 2;
-  print("The sum of no1 & no2 is ${no1 + no2}");
-  print("The subtraction of no1 & no2 is ${no1 - no2}");
-  print("The product of no1 & no2 is ${no1 * no2}");
-  print("The division of no1 & no2 is ${no1 / no2}");
-  print("The remainder of no1 & no2 is ${no2 % no1}");
-}
 
+// Syntax for Increment (pre-increment, post-increment) & Decrement (pre-decrement, post-decrement) operators
+  int a = 10;
+  
+  // Pre-increment
+  ++a;                   // a = a + 1
+  print(a);
+  // post-increment
+  a++;                   // a = a + 1
+  print(a);
+  
+  // Pre - decrement
+  --a;                   // a = a - 1
+  print(a);
+  
+  // post -decrement 
+  a--;                   // a = a - 1
+  print(a);
+
+}
