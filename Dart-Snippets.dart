8bdad3bf9455d6b5c0996bbe24eac8c4998fcbf4
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-11 12:19:51 +0500

    A simple dart funciton

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,9 +1,10 @@
-// Dart program to explain optional positional parameters
 void main() {
+  //Function calling
   addition(20, 40, 60, 80);
 }
 
-void addition(int no1, int no2, [int no3 = 0, int no4 = 0]) {
+// Function definition
+void addition(int no1, int no2, int no3, int no4) {
   int sum = no1 + no2 + no3 + no4;
   print(sum);
 }
