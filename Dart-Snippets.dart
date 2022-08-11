b4db6661109a7eac3ddc101999c96cb2594d1257
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-11 13:27:41 +0500

    Dart program to print returned value from a function

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,10 +1,10 @@
+// Dart program to print returned value from a function
 void main() {
   //Function calling
-  addition(20, 40, 60, 80);
+  print("The sum is ${addition(20, 40, 60, 80)}");
 }
 
 // Function definition
-void addition(int no1, int no2, int no3, int no4) {
-  int sum = no1 + no2 + no3 + no4;
-  print(sum);
+int addition(int no1, int no2, int no3, int no4) {
+  return no1 + no2 + no3 + no4;
 }
