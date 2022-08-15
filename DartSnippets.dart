775204aed1a65623953e009effa6477ab4d96924
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-15 17:06:19 +0500

    Different use cases to create a List in dart

--- DartSnippets.dart
+++ DartSnippets.dart
@@ -1,16 +1,17 @@
 void main() {
-  // Inferred List
+  // Inferred List using List literal '[]'
   var studentInfo = ["Ali", 45, 3.45, 34543454533];
 
   // The second way to create a list
   List studentInformation = ["Ali", 45, 3.45, 2324342342];
+  List<dynamic> studentInfo2 = ["Ali", 45, 3.45, 2324342342];
 
 // A way to create a statically typed List
   List<String> listOfNames = ["Ali", "Hamza", "Usman", "Sufyan"];
   List<int> listOfRollNos = [23, 42, 42, 56, 78, 32];
   List<double> listOfGPAs = [2.3, 3.4, 3.3, 2.23, 2.2, 3];
 
-// List declaration and initialization
+// Growable List or List declaration and initialization
   var list = []; // List declaration
 
   // List's initilization
@@ -20,21 +21,23 @@ void main() {
   list.add(false);
 
   List list2 = []; // List declaration
-
   // List's initilization
   list2.add("Ali");
   list2.add(45);
   list2.add(3.36);
   list2.add(false);
 
-  List<String> nameList = [];  // List declaration 
-  
+  List<String> nameList = []; // List declaration
+
   // List's initialization
   nameList.add("Ali");
   nameList.add("Hamza");
   nameList.add("Qais");
 
-  //How to visit a List through loop
+  // How to find the length of a List
+  print("The length of nameList is ${nameList.length}");
+
+  //How to visit a List through for-in loop
   for (String singleName in nameList) {
     print(singleName);
   }
