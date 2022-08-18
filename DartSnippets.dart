2518cc16773cf7a446658ed5a4bfd15df3ec8229
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-18 20:42:17 +0500

    OOP Definition

--- DartSnippets.dart
+++ DartSnippets.dart
@@ -1,44 +1,71 @@
 void main() {
-  // Inferred List using List literal '[]'
-  var studentInfo = ["Ali", 45, 3.45, 34543454533];
+  
+}
 
-  // The second way to create a list
-  List studentInformation = ["Ali", 45, 3.45, 2324342342];
-  List<dynamic> studentInfo2 = ["Ali", 45, 3.45, 2324342342];
 
-// A way to create a statically typed List
-  List<String> listOfNames = ["Ali", "Hamza", "Usman", "Sufyan"];
-  List<int> listOfRollNos = [23, 42, 42, 56, 78, 32];
-  List<double> listOfGPAs = [2.3, 3.4, 3.3, 2.23, 2.2, 3];
+// We Learnt: Procedural Programming 
+ // Topic: Object Oriented Programming ( OOP )
+ 
+ // OOP is not a language
+ // OOP is the name of structure
+ // OOP is the name of Approach
+ // OOP is the name of Pattern
+ // OOP is just a way of working
+ // Now onwards, our way of working will be different
+ // We will implement procedural concepts in OOP
+  
+  
+  
+//OOP Main Concepts
+// *Classes & Objects
+// *Constructors
+// *Inheritance
+// *Polymorphism
+// *Abstraction
+// *Encapsulation
+// *Generics
 
-// Growable List or List declaration and initialization
-  var list = []; // List declaration
 
-  // List's initilization
-  list.add("Ali");
-  list.add(45);
-  list.add(3.36);
-  list.add(false);
+// ========================
 
-  List list2 = []; // List declaration
-  // List's initilization
-  list2.add("Ali");
-  list2.add(45);
-  list2.add(3.36);
-  list2.add(false);
+//Classes & Objects
+// *What is the concept of classes & objects
+// -A Class represents a group 
+// -Classes can only have two things
+// (1) Functions / Methods 
+// (2) Properties / Fields / Attributes
 
-  List<String> nameList = []; // List declaration
 
-  // List's initialization
-  nameList.add("Ali");
-  nameList.add("Hamza");
-  nameList.add("Qais");
+//=========================
 
-  // How to find the length of a List
-  print("The length of nameList is ${nameList.length}");
+ // *How to create a class in Dart
+ // *In general, there are two thypes of classes, buil-in and user defined
+ // *We always use upper camel case convetion while creating classes
 
-  //How to visit a List through for-in loop
-  for (String singleName in nameList) {
-    print(singleName);
+class Student {
+ 
+// Class Properties / Fields / Attributes / variables
+ late String studentName;
+ late int studentRollNo;
+ late double studentGPA;
+ late bool isCheck;
+ var emailAddress;
+ var phoneNumer;
+    
+  
+// Functions / Methods
+
+  void printStudentDetails() {
+    
   }
+  
+  double gpaCalcuator ( ) {
+    
+    return 0.0;
+  } 
+  
+  performance () {
+    
+  }
+  
 }
