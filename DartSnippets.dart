0fece5e0168587008ce9f2494fe5f83e03f49041
Muhammad Afzal <imafzalakram@gmail.com>
2022-08-25 20:20:30 +0500

    // We can call parent constructors from child class's constructor, constructor-1 of cat class can call constructor-1 of animal and constructor-2 from cat class can call constructor-2 of animal class and so on

--- DartSnippets.dart
+++ DartSnippets.dart
@@ -1,11 +1,11 @@
 void main() {
-//  When there will be 'multiple-arguments' in Cat class and in Animal class then we must have to use super keyword iwth all child class constructors
+// We can call parent constructors from child class's constructor, constructor-1 of cat class can call constructor-1 of animal and constructor-2 from cat class can call constructor-2 of animal class and so on
   Map catDetails = {
     'cat_name': 'Simba',
     'cat_weight': 4,
     'cat_skincolor': 'light brown'
   };
-  
+
   Cat cat = Cat(4);
   Cat cat2 = Cat.details(catDetails);
 }
@@ -15,18 +15,20 @@ class Animal {
   Animal(int animalWeight) {
     print("Animal weight is $animalWeight");
   }
+
+  Animal.details(Map animalDetails) {
+    print("Animal name is ${animalDetails['cat_name']}");
+    print("Animal weight is ${animalDetails['cat_weight']}");
+    print("Animal skin color is ${animalDetails['cat_skincolor']}");
+  }
 }
 
-
 class Cat extends Animal {
-  
   Cat(int catWeight) : super(catWeight) {
     print("Cat weight is $catWeight");
   }
 
-  Cat.details(Map catDetails) : super(catDetails['cat_weight']) {
-    print("Cat name is ${catDetails['cat_name']}");
-    print("Cat weight is ${catDetails['cat_name']}");
-    print("Cat skin color is ${catDetails['cat_skincolor']}");
+  Cat.details(Map catDetails) : super.details(catDetails) {
+    print("Cat named class constructor is called!");
   }
 }
