5bcb7f38968c0fde8c2e6e4e61a2300e4fc39045
Muhammad Afzal <imafzalakram@gmail.com>
2022-10-19 21:01:07 +0500

    Variable declaration & initilization

--- DartSnippets.dart
+++ DartSnippets.dart
@@ -1,8 +1,23 @@
 void main() {
-  var name = 'Ali';
-  var rollNo = 45;
-  var GPA = 3.45;
-  var state = false;
+  // Variable declaration
+  var name;
+  // variable initizilation
+  name = 'Ali';
+
+  // variable declaration
+  var rollNo;
+  //variable initialzaiton
+  rollNo = 45;
+
+  //variable declaraiton
+  var GPA;
+  // variable initilization
+  GPA = 3.35;
+
+  //variable declaraiton
+  var state;
+  // variable initizilation
+  state = true;
 
   print(name);
   print(rollNo);
