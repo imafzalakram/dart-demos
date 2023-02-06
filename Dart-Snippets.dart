5f35cdb67f78517915ff1b3c2ee919a48caff2c4
Muhammad Afzal <77921765+imafzalakram@users.noreply.github.com>
2023-02-06 21:12:46 +0500

    Implementation of null aware operators

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,22 +1,4 @@
-enum Order { open, confirmed, pending, delivered, completed, canceled }
-
 void main() {
-  //case-1
-  print(Order.values);
-
-  //case-2
-  print(Order.open);
-  print(Order.confirmed);
-  print(Order.pending);
-  print(Order.delivered);
-  print(Order.canceled);
-  print(Order.completed);
-
-  //case-3
-  print(Order.open.name);
-  print(Order.confirmed.name);
-  print(Order.pending.name);
-  print(Order.delivered.name);
-  print(Order.canceled.name);
-  print(Order.completed.name);
+  int? a;
+  print(a ?? 10);
 }
