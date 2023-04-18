de441ea5c8f1a8779fd275ce654d3d00cdb8ed1c
Muhammad Afzal <77921765+imafzalakram@users.noreply.github.com>
2023-04-18 12:28:25 +0500

    Update Dart-Snippets.dart

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,4 +1,10 @@
 void main() {
-  // single-line comment
-  /* Multiline comment */
+  // The following line of code will print a text on console
+  print("My name is Khan");
+
+  /*
+   We are currently learning Dart code 
+   because we need to work on the
+   Flutter framework
+   */
 }
