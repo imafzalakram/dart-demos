466fd80286c003796218b511420268274e303b6f
vickykhan2 <130727716+vickykhan2@users.noreply.github.com>
2023-04-19 10:07:39 +0500

    Added a comment about numbers in dart.

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,7 +1,8 @@
 void main() {
   // The following line of code will print a text on console
   print("My name is Khan");
-
+// When we write a number then we don't need double quotes as 
+  print (4);
   /*
    We are currently learning Dart code 
    because we need to work on the
