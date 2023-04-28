f683d1ff190ff22f8d73bfd1896a062bcad02445
imafzalakram <77921765+imafzalakram@users.noreply.github.com>
2023-04-28 20:42:14 +0500

    dynamic keyword explanation

--- Dart-Snippets.dart
+++ Dart-Snippets.dart
@@ -1,13 +1,13 @@
 void main() {
-  // ‘const’ keyword is used to createa a compile-time constant.
-  // We should use 'const' keyword when the value is known at compile time.
-  const pi = 3.14;
-  const g = 9.8;
+  // 'dynamic' is a type on top of all other data-types
+  // ‘dynamic’ is a special type indicating it can be any type.
+  // variables of type ‘dynamic’ can change their data-type, int, double, String
 
-  // ‘final’ keyword is used to create a runtime constant.
-  // ‘final’ keyword should be used over ‘const’ if you don't know the value at compile time
-  final pi_;
-  pi_ = 3.14;
-  final g_;
-  g_ = 9.8;
+  dynamic a = 0.0;
+
+  a = 10;
+  print(a);
+
+  a = "Ali";
+  print(a);
 }
